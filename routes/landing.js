fc1c7057c9ec1d726d831e4f36a6dcb80c915805
Karim Hesham <karimdeveloper@outlook.com>
2021-01-29 18:31:04 +0200

    updated server.js

--- routes/landing.js
+++ routes/landing.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
 
 const path = require('path');
-const filePath = "./client/public/index.html";
+const filePath = "../build/index.html";
 const resolvedPath = path.resolve(filePath);
 
 // console.log(resolvedPath);
 
-// router.route("*").get((req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
-// console.log(__dirname);
+router.route("/").get((req, res) => {
+    res.sendFile(filePath);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -70,11 +70,11 @@ passport.deserializeUser(User.deserializeUser());
 //   });
 // }
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static("./build/index.html"));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile("./build/index.html");
 });
 
 app.use("/", notesRouter);
