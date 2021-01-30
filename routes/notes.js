54987c622309a4aa5aa6a2fca385575b58ef748d
Karim Hesham <karimdeveloper@outlook.com>
2021-01-30 16:07:46 +0200

    rollback notes route

--- routes/notes.js
+++ routes/notes.js
@@ -4,10 +4,9 @@ let Note = require("../models/notes");
 
 router.route("/notes/:username").get((req, res) => {
     
-    const notes = await Note.find({username: req.params.username});
-    res.send(notes);
-    // .then(notes => res.send(notes))
-    // .catch(err => res.status(400).json("Error" + err));
+    Note.find({username: req.params.username})
+    .then(notes => res.send(notes))
+    .catch(err => res.status(400).json("Error" + err));
      
 });
 
