edf5432bc01519add6547f833564da2c98214d23
Karim Hesham <karimdeveloper@outlook.com>
2021-02-04 13:12:59 +0200

    Fixed syntax error in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const logoutRouter = require("./routes/logout");
 require("dotenv").config();
 
 const app = express();
-const port = procces.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
